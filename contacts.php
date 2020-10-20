9585a56c5ccf9e8a89d52cdfcece517f838a2dd0
gabrainmonke <123ri.ga456@gmail.com>
2020-10-20 08:39:29 +0200

    update 7

--- contacts.php
+++ contacts.php
@@ -1,19 +1,21 @@
 <?php include_once 'header.php' ?>
 
-<div class="aboutMe">
-    <div class="myImg">
-        <img src="img/mmmmmmm.jpg" alt="myImage">
+    <div class="aboutMe">
+
+        <div class="myImg">
+            <img src="img/mmmmmmm.jpg" alt="myImage">
+        </div>
+
+        <div class="myInfo">
+            <p>
+                Hello my name is Richard Gabarík, and this beautiful web"art" is my way of polishing my skill for future
+            </p>
+            <p>
+                Email : risingWebDeveloper@webMonkeh.com <br>
+                Contact : *************************
+            </p>
+        </div>
     </div>
-    <div class="myInfo">
-        <p>
-            Hello my name is Richard Gabarík, and this beautiful web"art" is my way of polishing my skill for future
-        </p>
-        <p>
-            Email : risingWebDeveloper@webMonkeh.com <br>
-            Contact : *************************
-        </p>
-    </div>
-</div>
 
 
 <?php include_once 'footer.php' ?>
